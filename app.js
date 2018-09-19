41d2ad72563b02b4b77d0892189e936ace30a2eb
shadish <shadtisch@gmail.com>
2018-09-18 22:35:23 -0600

    add stats

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('foo'))
+const getRandomArbitrary = (min, max) => {
+	const result = Math.random() * (max - min) + min;
+	return Math.round(result);
+}
+
+
+const getResponse = () => {
+	return {
+		INT: getRandomArbitrary(3,18),
+		DEX: getRandomArbitrary(3,18),
+		STR: getRandomArbitrary(3,18),
+		WIS: getRandomArbitrary(3,18),
+		CON: getRandomArbitrary(3,18),
+		CHR: getRandomArbitrary(3,18)
+	}
+}
+
+app.get('/', (req, res) => {
+	const response = getResponse();
+	res.send(response);
+})
 
 app.listen(3000, () => {
 	console.log('express is running on port 3000')
