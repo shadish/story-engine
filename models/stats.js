a0dd9b25e80075773dab42c6f56937b15230cabd
shadish <shadtisch@gmail.com>
2018-09-25 23:49:53 -0600

    refactor stat constants

--- models/stats.js
+++ models/stats.js
@@ -1,13 +1,16 @@
 const { randomFromRange } = require('../utils/randomHelpers')
 
+const MIN_STAT = 3
+const MAX_STAT = 18
+
 const rollStats = () => {
 	return {
-		INT: randomFromRange(3, 18),
-		DEX: randomFromRange(3, 18),
-		STR: randomFromRange(3, 18),
-		WIS: randomFromRange(3, 18),
-		CON: randomFromRange(3, 18),
-		CHR: randomFromRange(3, 18)
+		INT: randomFromRange(MIN_STAT, MAX_STAT),
+		DEX: randomFromRange(MIN_STAT, MAX_STAT),
+		STR: randomFromRange(MIN_STAT, MAX_STAT),
+		WIS: randomFromRange(MIN_STAT, MAX_STAT),
+		CON: randomFromRange(MIN_STAT, MAX_STAT),
+		CHR: randomFromRange(MIN_STAT, MAX_STAT)
 	}
 }
 
